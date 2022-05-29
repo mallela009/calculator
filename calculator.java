35edce659204f4754730dfc79e5a04818d62c899
mallela009 <92942717+mallela009@users.noreply.github.com>
2022-05-29 05:09:28 +0000

    first commit
    this first version

---
+++ calculator.java
@@ -0,0 +1,13 @@
+public class calculator{
+ public static void main(String [] args) {
+     int num1 = 34;
+     int num2 = 45;
+     int total= num1+num2;
+     System.out.println("the sum is "+total);
+     
+
+
+
+    
+ }   
+}
